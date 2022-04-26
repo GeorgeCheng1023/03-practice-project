ab23e3101b2b8e7d803d20cdd464265a98bdc50e
GeorgeCheng1023 <ck40292@gmail.com>
2022-04-26 21:58:29 +0800

    add header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton';
+const Header = props => {
+  return (
+    <>
+  <header className={classes['header']}>
+    <h1>Order your meal</h1>
+    <HeaderCartButton/>
+  </header>
+  <div className={classes['main-image']}>
+    <img src="https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true"></img>
+  </div>
+  </>
+  )
+}
+
+export default Header;
