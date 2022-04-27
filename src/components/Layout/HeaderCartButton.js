18dd0ce59769bf2b71b7a0d1cb9edbb3ac485f7b
GeorgeCheng1023 <ck40292@gmail.com>
2022-04-28 01:00:18 +0800

    add cart button bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,11 +1,29 @@
-import {useContext} from 'react'
+import {useContext, useState, useEffect} from 'react'
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 import CartContext from '../../contexts/CartContext'
 const HeaderCartButton = props => {
   const cartContext = useContext(CartContext);
+  const [isHightLightButton, setIsHightLightButton] = useState(false);
 
-  return (<button className={classes.button} onClick={cartContext.onShow}>
+  useEffect(() => {
+    if(cartContext.items.length === 0) {
+      return;
+    }
+    setIsHightLightButton(true);
+    const timer = setTimeout(() => {
+      setIsHightLightButton(false);
+    }, 300)
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [cartContext.items]
+  )
+
+  return (
+  <button 
+    className={`${classes.button} ${isHightLightButton && classes.bump}`} 
+    onClick={cartContext.onShow}>
     <span  className={classes.icon} > <CartIcon/></span>
     <span> Your Cart </span>
     <span className={classes.badge}>{cartContext.totalAmount}</span>
